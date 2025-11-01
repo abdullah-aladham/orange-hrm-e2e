f42dc226f51fbd367a8f5e5f7470005a0abe72d6
abdullah-aladham <abdullah.aladham@gmail.com>
2025-11-02 00:24:57 +0200

    added some test cases

--- cypress/e2e/specs/buzz-spec.cy.ts
+++ cypress/e2e/specs/buzz-spec.cy.ts
@@ -23,57 +23,40 @@ cy.fixture('buzz').then((buzzes)=>{
    });
 
  });
+it('comments on a post',()=>{
 
+})
 
+ it('likes a post',()=>{
+
+ })
+it('adds 5 likes for a post',()=>{
+
+})
 // });
 // const datafile = path.join(__dirname, "../../fixtures/buzz-comments.json");
 // const buzzComments = JSON.parse(cy.readFile(datafile, "utf-8"));
-//   it('comments on buzzes',()=>{
-//    cy.fixture('buzz-comments').then((comments)=>{
-//   comments.forEach((comment) => {
+   it('comments on buzzes',()=>{
+    cy.fixture('buzz-comments').then((comments)=>{
+   //comments.forEach((comment) => {
 // //    it(buzz.Test_Name,()=>{
-//      adminpageobj.NavigateToBuzz();
-//      buzzpageobj.createApost(comment.Comment);
+      adminpageobj.NavigateToBuzz();
+      buzzpageobj.createApost(comments.Comment);
 
 
-//    });
+  });
 
 })
 // cy.fixture('buzz-comments').then((comments)=>{
 //  comments.forEach((buzz_comment) => {
 
 });
+it('comments 5 times on a post',{retries:5},()=>{
+cy.fixture('buzz-comments').then((comments)=>{
+   //comments.forEach((comment) => {
+// //    it(buzz.Test_Name,()=>{
+      adminpageobj.NavigateToBuzz();
+      buzzpageobj.createApost(comments.Comment);
 
-// })
-// });
-// });
-// // })/* ==== Test Created with Cypress Studio ==== */
-// it('add-buzz', function() {
-//   /* ==== Generated with Cypress Studio ==== */
-//   cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-//   cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').clear('ِ');
-//   cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin');
-//   cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').clear();
-//   cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123');
-//   cy.get('.oxd-button').click();
-//   cy.get(':nth-child(12) > .oxd-main-menu-item > .oxd-text').click();
-//   cy.get('.oxd-buzz-post-input').click();
-//   cy.get('.oxd-buzz-post-slot > .oxd-button').click();
-//   cy.should('be.visible');
-//   /* ==== End Cypress Studio ==== */
-// });/* ==== Test Created with Cypress Studio ==== */
-// it('buzztest', function() {
-//   /* ==== Generated with Cypress Studio ==== */
-//   cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-//   cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').clear('A');
-//   cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin');
-//   cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').clear();
-//   cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123');
-//   cy.get('.oxd-button').click();
-//   cy.get(':nth-child(12) > .oxd-main-menu-item').click();
-//   cy.get('.oxd-buzz-post-input').click();
-//   cy.get('.oxd-buzz-post-input').type('hello')
-//   cy.get('.oxd-buzz-post-slot > .oxd-button').click();
-//   cy.get('.oxd-toast-content').should('be.visible');
-//   /* ==== End Cypress Studio ==== */
-// });
+})
+});
